8af1e8c3de31af31100f56de89512c9571fc1cbf
ke-ke-z <kazuharu20010122@gmail.com>
2021-12-10 15:04:00 +0900

    no message

--- JointDev/JointDev/View/Main/Child/keke/KekeViewController.swift
+++ JointDev/JointDev/View/Main/Child/keke/KekeViewController.swift
@@ -7,5 +7,26 @@
 
 import UIKit
 
-class KekeViewController: 
+class KekeViewController: UIViewController {
+//    let alarm = Alarm()
+    @IBOutlet weak var sleepTimePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //UIDatePickerを.timeモードにする
+        sleepTimePicker.datePickerMode = UIDatePicker.Mode.time
+        //現在の時間をDatePickerに表示
+        sleepTimePicker.setDate(Date(), animated: false)
+    }
+    
+}
 
+func setButton(_ sender: UIButton) {
+}
+
+extension KekeViewController {
+    static func instantiate() -> Self {
+        let vc: KekeViewController = .loadFromStoryboard()
+        return vc as! Self
+    }
+}
